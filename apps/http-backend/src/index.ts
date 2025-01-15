b82a97821b63e761962b5bb81a466ebb26ae0ec1
Vineet Chelani <vineetchelani545@gmail.com>
2025-01-15 14:51:03 +0530

    added schemas in prisma package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import {client}  from "@repo/db/client";
 const app = express();
 app.listen(8000, () => {        
   console.log("Server is running on port 8000");
